8977bf51160dd7d41a915331ea6ef2f7bf16e478
Bpkreddy022 <praveenaws0622@gmail.com>
2025-08-06 19:06:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,31 @@ public class AsciiValue {
         System.out.println("The ASCII value of " + ch + " is: " + castAscii);
     }
 }
+
+	Subtraction
+*	Multiplication
+/	Division
+%	Modulo Operation (Remainder after division)
+Example 1: Arithmetic Operators
+class Main {
+  public static void main(String[] args) {
+    
+    // declare variables
+    int a = 12, b = 5;
+
+    // addition operator
+    System.out.println("a + b = " + (a + b));
+
+    // subtraction operator
+    System.out.println("a - b = " + (a - b));
+
+    // multiplication operator
+    System.out.println("a * b = " + (a * b));
+
+    // division operator
+    System.out.println("a / b = " + (a / b));
+
+    // modulo operator
+    System.out.println("a % b = " + (a % b));
+  }
+}
